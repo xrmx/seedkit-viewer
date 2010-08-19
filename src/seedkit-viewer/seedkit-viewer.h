37cdeaa4f83a58964776689f31bb9d87cfe46ae8
Alexandre Mazari <scaroo@gmail.com>
2010-08-19 19:48:41 +0200

    Fix dist-check issues. Change licence headers to the GPL3 licence. Point copyright email addess to mailing list

--- src/seedkit-viewer/seedkit-viewer.h
+++ src/seedkit-viewer/seedkit-viewer.h
@@ -1,19 +1,19 @@
 /* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
 /*
  * seedkit-viewer.h
- * Copyright (C) The SeedKit team. 2010 <scaroo@gmail.com>
+ * Copyright (C) The SeedKit team. 2010 <seedkit-list@gnome.org>
  * 
  * SeedKit is free software: you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
+ * under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
  * SeedKit is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU Lesser General Public License for more details.
+ * See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef _SEEDKIT_VIEWER_H
--- src/seedkit-viewer/seedkit-webkit.h
+++ src/seedkit-viewer/seedkit-webkit.h
@@ -1,1 +1,20 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*                                                                          
+ * seedkit-inspector.h                                                         
+ * Copyright (C) The SeedKit team. 2010 <seedkit-list@gnome.org>                     
+ *                                                                             
+ * SeedKit is free software: you can redistribute it and/or modify it          
+ * under the terms of the GNU General Public License as published              
+ * by the Free Software Foundation, either version 3 of the License, or        
+ * (at your option) any later version.                                         
+ *                                                                             
+ * SeedKit is distributed in the hope that it will be useful, but              
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                        
+ * See the GNU General Public License for more details.                        
+ *                                                                             
+ * You should have received a copy of the GNU General Public License           
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.       
+ */ 
+
 GtkWidget* create_web_view(gchar* file_uri, gchar* script_uri);
