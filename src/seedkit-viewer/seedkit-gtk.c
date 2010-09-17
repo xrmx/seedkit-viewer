af9e4d27b95bc7b971eb61e55a55465c93f94196
Alexandre Mazari <scaroo@gmail.com>
2010-09-17 10:40:25 +0200

    Clean up repository of generated files Add man page

--- src/seedkit-viewer/seedkit-gtk.c
+++ src/seedkit-viewer/seedkit-gtk.c
@@ -56,7 +56,7 @@ create_window (gchar* file_uri, SeedKitViewerSettings* settings)
 
 	window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
 
-	gtk_window_set_default_size (GTK_WINDOW(window), 800, 600);
+	gtk_window_set_default_size (GTK_WINDOW(window), 600, 300);
 	g_signal_connect (G_OBJECT (window), "destroy",
                       G_CALLBACK (destroy), NULL);
 	GtkWidget* vbox = gtk_vpaned_new();
